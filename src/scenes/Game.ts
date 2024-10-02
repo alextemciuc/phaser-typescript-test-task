f4f0c18e2359404ecbcdea6d9bf5848d58dd6a8e
Alex Temciuc <alexandru.temciuc@gmail.com>
2024-10-02 15:08:07 +0300

    first commit

---
+++ src/scenes/Game.ts
@@ -0,0 +1,19 @@
+import { Scene } from 'phaser';
+
+export class Game extends Scene
+{
+    constructor ()
+    {
+        super('Game');
+    }
+
+    preload()
+    {
+        
+    }
+
+    create ()
+    {
+        
+    }
+}
