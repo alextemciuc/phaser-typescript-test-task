7ce5ea418dad505698f92a27b81cae3568a9e0db
Alex Temciuc <alexandru.temciuc@gmail.com>
2024-10-24 16:14:43 +0300

    Main assets has been added to the scene. Functionality to create reels has been created.

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,7 +1,31 @@
-import { Scene } from 'phaser';
+import { Scene, GameObjects } from 'phaser';
+
+enum ESymbols {
+    BANANA = "banana",
+    CHERRY = "cherry",
+    BLACKBERRY = "blackberry"
+}
+
+const maxSymbols: number = 3;
+const minReelElements: number = 3;
+const maxReelElements: number = 6;
+const reelsNumber: number = 3;
+const initPositionX: number = -200;
+const initPositionY: number = -200;
+
+// const reels: ESymbols[][] = [
+//     [ESymbols.BANANA, ESymbols.BANANA, ESymbols.BLACKBERRY, ESymbols.CHERRY, ESymbols.CHERRY],
+//     [ESymbols.BLACKBERRY, ESymbols.BANANA, ESymbols.BLACKBERRY, ESymbols.CHERRY],
+//     [ESymbols.BANANA, ESymbols.CHERRY, ESymbols.BLACKBERRY, ESymbols.BANANA, ESymbols.BLACKBERRY, ESymbols.BANANA]
+// ]
 
 export class Game extends Scene
 {
+    background: GameObjects.Image;
+    title: GameObjects.Text;
+    spinButton: GameObjects.Image;
+    reels: ESymbols[][];
+
     constructor ()
     {
         super('Game');
@@ -9,11 +33,93 @@ export class Game extends Scene
 
     preload()
     {
-        
+        this.load.image('background', 'assets/Background.png');
+        this.load.image('banana', 'assets/Banana.png');
+        this.load.image('blackberry', 'assets/Blackberry.png');
+        this.load.image('cherry', 'assets/Cherry.png');
+        this.load.image('spinButton', 'assets/Spin.png');
     }
 
     create ()
     {
-        
+        this.background = this.add.image(this.cameras.main.width / 2, this.cameras.main.height / 2, 'background');
+        this.background.setScale(0.5);
+
+        this.title = this.add.text(512, 50, 'Game', {
+            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
+            stroke: '#000000', strokeThickness: 8,
+            align: 'center'
+        }).setOrigin(0.5);
+
+        this.spinButton = this.add.image(this.cameras.main.width / 2, this.cameras.main.height / 2 + 200, 'spinButton');
+        this.spinButton.setScale(0.75);
+        this.spinButton.setInteractive({ cursor: "pointer" });
+
+        this.reels = this.createReels();
+        const newReels = this.addSymbols(this.reels);
+        console.log(newReels);
+    }
+
+    createReels() {
+        const reels: ESymbols[][] = new Array(reelsNumber)
+        for (let i: number = 0; i < reels.length; i++) {
+            const reelDimension: number = this.getRandomReelDimension(minReelElements, maxReelElements);
+            reels[i] = new Array(reelDimension);
+        }
+
+        for (let i: number = 0; i < reels.length; i++) {
+            for (let j: number = 0; j < reels[i].length; j++) {
+                const randomInt = this.getRandomSymbolNumber(maxSymbols);
+                switch (randomInt) {
+                    case 0:
+                        reels[i][j] = ESymbols.BANANA;
+                        break;
+                    case 1:
+                        reels[i][j] = ESymbols.BLACKBERRY;
+                        break;
+                    case 2:
+                        reels[i][j] = ESymbols.CHERRY;
+                        break;
+                }
+            }
+        }
+        return reels;
+    }
+
+    addSymbols(reelsString: ESymbols[][]) {
+        let positionX: number = initPositionX;
+        const newReels: GameObjects.Image[][] = reelsString.map((column: ESymbols[]) => {
+            let positionY: number = initPositionY;
+            const newColumn: GameObjects.Image[] = column.map((symbol: ESymbols) => {
+                let image: GameObjects.Image;
+                switch (symbol) {
+                    case ESymbols.BANANA:
+                        image = this.add.image(this.cameras.main.width / 2 + positionX, this.cameras.main.height / 2 + positionY, symbol).setScale(0.5);
+                        break;
+                    case ESymbols.BLACKBERRY:
+                        image = this.add.image(this.cameras.main.width / 2 + positionX, this.cameras.main.height / 2 + positionY, symbol).setScale(0.5);
+                        break;
+                    case ESymbols.CHERRY:
+                        image = this.add.image(this.cameras.main.width / 2 + positionX, this.cameras.main.height / 2 + positionY, symbol).setScale(0.5);
+                        break;
+                }
+                positionY += Math.abs(initPositionY);
+                return image;
+            });
+            positionX += Math.abs(initPositionX);
+            return newColumn;
+        });
+        return newReels;
+    }
+
+    getRandomSymbolNumber(max: number) {
+        return Math.floor(Math.random() * max);
+    }
+
+    getRandomReelDimension(min: number, max: number) {
+        const minCeiled: number = Math.ceil(min);
+        const maxFloored: number = Math.floor(max);
+
+        return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
     }
 }
