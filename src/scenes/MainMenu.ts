06363ec5779d616fb9be9451886b76fbb504431a
Alex Temciuc <alexandru.temciuc@gmail.com>
2024-10-24 16:09:38 +0300

    Scene has been set to start the Game scene. Also, title has been added.

--- src/scenes/MainMenu.ts
+++ src/scenes/MainMenu.ts
@@ -1,7 +1,9 @@
-import { Scene } from 'phaser';
+import { Scene, GameObjects } from 'phaser';
 
 export class MainMenu extends Scene
 {
+    title: GameObjects.Text;
+
     constructor ()
     {
         super('MainMenu');
@@ -9,6 +11,16 @@ export class MainMenu extends Scene
 
     create ()
     {
+        this.title = this.add.text(512, 460, 'Main Menu', {
+            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
+            stroke: '#000000', strokeThickness: 8,
+            align: 'center'
+        }).setOrigin(0.5);
         
+        this.input.once('pointerdown', () => {
+
+            this.scene.start('Game');
+
+        });
     }
 }
