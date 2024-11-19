f7e8976d650c7166aa753261b1f7c1d66b1e0f46
Alex Temciuc <alexandru.temciuc@gmail.com>
2024-11-19 11:07:40 +0200

    Constants have been added.

---
+++ src/constants/constants.ts
@@ -0,0 +1,13 @@
+
+const constants = {
+  maxSymbols: 3,
+  minReelElements: 3,
+  maxReelElements: 6,
+  reelsNumber: 3,
+  initPositionX: -200,
+  initPositionY: -200,
+  minSpinningTime: 1,
+  maxSpinningTime: 3
+}
+
+export default constants;
