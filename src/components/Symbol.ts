08ddd0c9ac9f313cf96a65f8d7d6d9c6089159d7
Alex Temciuc <alexandru.temciuc@gmail.com>
2024-11-19 11:16:17 +0200

    Symbol class has been created.

---
+++ src/components/Symbol.ts
@@ -0,0 +1,8 @@
+
+export class Symbol extends Phaser.GameObjects.Image
+{
+  constructor(scene: Phaser.Scene, x: number, y: number, texture: string) {
+    super(scene, x, y, texture);
+    scene.add.existing(this);
+  }
+}
